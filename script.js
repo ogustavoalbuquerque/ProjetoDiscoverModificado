d7d16eeb72a2d8f5d687de84abbc92b02e2fd635
Gustavo Henrique <gustavo.henrique.g12@hotmail.com>
2023-10-31 14:22:53 -0300

    customization

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("Light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar2-light.png")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar2.png")
   }
 }
